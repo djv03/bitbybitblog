e2595b19a219ccf0aab2550f22b23803c7864280
dhruvin <dhruvin.200410116086@gmail.com>
2023-04-02 23:13:34 +0530

    Navabar made global

---
+++ Learnings/about _app.js/globalComponent.js
@@ -0,0 +1,14 @@
+//this is very crucial to me because this is somethig i have been looking for in my react app and couldnt do still
+
+//but i have been able to do it in my nextjs app
+//lets see how !
+
+//first of all we need to talk about the _app.js file
+//this file is the main file in nextjs and executes every time a page is loaded/refreshed/changed
+
+//you can check it out by adding a console.log statement in the _app.js file
+
+//so for the a componet to be global it has to be in the _app.js file like navabr
+
+//so lets create a global component
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+
+import styles from '@/styles/Home.module.css'
+
+function Navbar() {
+  return (
+    <div>
+         <div className={styles.navbar}>
+          <Link href='/'><h1> bitbybit.com</h1></Link>
+          <ul >
+            <Link href='/'><li className="navlinks">Home</li> </Link>
+            <Link href='/about'><li className="navlinks">About</li> </Link>
+            <Link href='/contact'><li className="navlinks">contact</li> </Link>
+            <Link href='/pro'><li className="navlinks">pro</li> </Link>
+          </ul>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
