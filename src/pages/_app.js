eff112974d54ab9e8cdcb3782b715f293f2d53b7
dhruvin <dhruvin.200410116086@gmail.com>
2023-04-04 14:26:21 +0530

    endpoints implemented

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import Navbar from '@/components/Navbar'
+import './blogposts/slug.css'
 
 export default function App({ Component, pageProps }) {
   return (
---
+++ src/pages/api/blogs.js
@@ -0,0 +1,7 @@
+import * as fs from 'fs';
+
+export default function handler(req, res) {
+  fs.readdir("blogdata",'utf-8', (err, data)=>{
+    console.log(data);
+    res.status(200).json(data)
+  })}
